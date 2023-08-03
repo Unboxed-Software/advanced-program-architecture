2be13dd8c907b5347c868e6f995b2c00d1c824ac
Coach Chuck <christian.krueger.health@gmail.com>
2023-08-03 15:12:10 -0600

    working

--- programs/architecture/src/tutorial/rpg.rs
+++ programs/architecture/src/tutorial/rpg.rs
@@ -1,6 +1,12 @@
 use anchor_lang::prelude::*;
 use anchor_lang::system_program::{Transfer, transfer};
 
+// SOLUTION EDIT: Added in an item drop
+const ITEM_NAME: [u8; 32] = [
+    b'b', b'r', b'o', b'n', b'z', b'e', b',', b' ', b's', b'p', b'e', b'a', b'r',
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+];
+
 #[account]
 pub struct Game { // 8 bytes
     pub game_master: Pubkey,            // 32 bytes
@@ -46,7 +52,14 @@ pub struct Monster { // 8 bytes
 #[derive(Clone, AnchorSerialize, AnchorDeserialize)]
 pub struct GameConfig {
     pub max_items_per_player: u8,
-    pub for_future_use: [u64; 16], // Health of Enimies?? Experince per item?? Action Points per Action??
+    
+    // SOLUTION EDIT:
+    pub ap_per_player_creation: u64,
+    pub ap_per_monster_spawn: u64,
+    pub ap_per_monster_attack: u64,
+
+    // SOLUTION EDIT: 16 -> 13
+    pub for_future_use: [u64; 13], // Health of Enimies?? Experince per item?? Action Points per Action??
 }
 
 // ----------- STATUS ----------
@@ -55,7 +68,7 @@ pub struct GameConfig {
 // const IS_POISONED_FLAG: u8 = 1 << 1;
 // const IS_BURNING_FLAG: u8 = 1 << 2;
 // const IS_BLESSED_FLAG: u8 = 1 << 3;
-// const IS_CURSED_FLAG: u8 = 1 << 4;
+const IS_CURSED_FLAG: u8 = 1 << 4;
 // const IS_STUNNED_FLAG: u8 = 1 << 5;
 // const IS_SLOWED_FLAG: u8 = 1 << 6;
 // const IS_BLEEDING_FLAG: u8 = 1 << 7;
@@ -115,13 +128,25 @@ pub struct CreateGame<'info> {
     pub system_program: Program<'info, System>,
 }
 
-pub fn run_create_game(ctx: Context<CreateGame>, max_items_per_player: u8) -> Result<()> {
+
+pub fn run_create_game(
+    ctx: Context<CreateGame>, 
+    max_items_per_player: u8, 
+    // SOLUTION EDIT: added game config ( could make this a struct )
+    ap_per_player_creation: u64,
+    ap_per_monster_spawn: u64,
+    ap_per_monster_attack: u64,
+) -> Result<()> {
 
     ctx.accounts.game.game_master = ctx.accounts.game_master.key().clone();
     ctx.accounts.game.treasury = ctx.accounts.treasury.key().clone();
 
     ctx.accounts.game.action_points_collected = 0;
     ctx.accounts.game.game_config.max_items_per_player = max_items_per_player;
+    // SOLUTION EDIT:
+    ctx.accounts.game.game_config.ap_per_player_creation = ap_per_player_creation;
+    ctx.accounts.game.game_config.ap_per_monster_spawn = ap_per_monster_spawn;
+    ctx.accounts.game.game_config.ap_per_monster_attack = ap_per_monster_attack;
 
     msg!("Game created!");
 
@@ -163,8 +188,9 @@ pub fn run_create_player(ctx: Context<CreatePlayer>) -> Result<()> {
 
     msg!("Hero has entered the game!");
 
-    {   // Spend 100 lamports to create player
-        let action_point_to_spend = 100;
+    {
+        // SOLUTION EDIT:
+        let action_point_to_spend = ctx.accounts.game.game_config.ap_per_player_creation;
 
         spend_action_point(
             action_point_to_spend, 
@@ -224,8 +250,9 @@ pub fn run_spawn_monster(ctx: Context<SpawnMonster>) -> Result<()> {
         ctx.accounts.player_account.next_monster_index = ctx.accounts.player_account.next_monster_index.checked_add(1).unwrap();
     }
 
-    {   // Spend 5 lamports to spawn monster
-        let action_point_to_spend = 5;
+    { 
+        // SOLUTION EDIT:
+        let action_point_to_spend = ctx.accounts.game.game_config.ap_per_monster_spawn;
 
         spend_action_point(
             action_point_to_spend, 
@@ -242,10 +269,14 @@ pub fn run_spawn_monster(ctx: Context<SpawnMonster>) -> Result<()> {
 // ----------- ATTACK MONSTER ----------
 #[derive(Accounts)]
 pub struct AttackMonster<'info> {
+     // SOLUTION EDIT:
+    pub game: Box<Account<'info, Game>>,
 
     #[account(
         mut,
         has_one = player,
+        // SOLUTION EDIT:
+        has_one = game,
     )]
     pub player_account: Box<Account<'info, Player>>,
 
@@ -282,6 +313,9 @@ pub fn run_attack_monster(ctx: Context<AttackMonster>) -> Result<()> {
             msg!("Damage Dealt: {}", damage_dealt);
         } else {
             msg!("Stop it's already dead!");
+
+            // SOLUTION EDIT:
+            ctx.accounts.player_account.status_flag |= IS_CURSED_FLAG;
         }
     }
 
@@ -292,12 +326,27 @@ pub fn run_attack_monster(ctx: Context<AttackMonster>) -> Result<()> {
         if did_kill {
             ctx.accounts.player_account.kills = ctx.accounts.player_account.kills.saturating_add(1);
             msg!("You killed the monster!");
+
+            // SOLUTION EDIT:
+            if ctx.accounts.player_account.inventory.len() < ctx.accounts.game.game_config.max_items_per_player as usize {
+                ctx.accounts.player_account.inventory.push(
+                    InventoryItem { 
+                        name: ITEM_NAME.clone(), 
+                        amount: 1, 
+                        for_future_use: [0; 128]
+                    }
+                );
+                msg!("You looted the monster!");
+            } else {
+                msg!("You can't loot the monster, your inventory is full!");
+            }
         }
     }
 
 
-    {   // Spend 1 lamports to attack monster
-        let action_point_to_spend = 1;
+    { 
+        // SOLUTION EDIT:
+        let action_point_to_spend = ctx.accounts.game.game_config.ap_per_monster_attack;
 
         spend_action_point(
             action_point_to_spend, 
