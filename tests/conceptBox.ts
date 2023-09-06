c9cb0f470cc53e2301a5c4aa05a300256967021f
James Pacheco <jamesrp13@gmail.com>
2023-09-06 10:48:47 -0600

    Minor updates

--- tests/conceptBox.ts
+++ tests/conceptBox.ts
@@ -1,41 +1,35 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Architecture } from "../target/types/architecture";
-import { printLogInstruction } from "./utils";
-import { assert } from "chai";
+import * as anchor from "@coral-xyz/anchor"
+import { Program } from "@coral-xyz/anchor"
+import { Architecture } from "../target/types/architecture"
+import { printLogInstruction } from "./utils"
+import { assert } from "chai"
 
-
-describe("Concept Box", () => {
+describe("Concept: Box", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(anchor.AnchorProvider.env())
 
-  const program = anchor.workspace.Architecture as Program<Architecture>;
-  const wallet = anchor.workspace.Architecture.provider.wallet.payer as anchor.web3.Keypair;
+  const program = anchor.workspace.Architecture as Program<Architecture>
+  const wallet = anchor.workspace.Architecture.provider.wallet
+    .payer as anchor.web3.Keypair
 
   it("Exploring Box", async () => {
+    const someBigData = anchor.web3.Keypair.generate()
 
-    const someBigData = anchor.web3.Keypair.generate();
+    const txHash = await program.methods
+      .conceptBox()
+      .accounts({
+        owner: wallet.publicKey,
+        someBigData: someBigData.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([someBigData])
+      .rpc()
 
-      const txHash = await program.methods.conceptBox(
-    ).accounts({
-      owner: wallet.publicKey,
-      someBigData: someBigData.publicKey,
-      systemProgram: anchor.web3.SystemProgram.programId,
-    }).signers([
-      someBigData,
-    ])
-    .rpc()
-
-    await program.provider.connection.confirmTransaction(txHash);
+    await program.provider.connection.confirmTransaction(txHash)
 
     // Print out if you'd like
     // const account = await program.account.someBigDataStruct.fetch(someBigData.publicKey);
 
-    printLogInstruction('box', txHash);
-
-  });
-
-});
-
-
-
+    printLogInstruction("box", txHash)
+  })
+})
